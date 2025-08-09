b7e883ed313143742c71b444b5cb0278b5d5d78c
Andres <andres.ff.silva@gmail.com>
2025-08-09 19:03:26 +0000

    Make the change

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World on the World!");
 
 app.Run();
